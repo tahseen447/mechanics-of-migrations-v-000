491a8d18cc46bdee31657979e6002288d12814d7
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-22 22:53:10 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,10 @@
 class CreateArtists < ActiveRecord::Migration
+
+  connection = ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
+
   def up
   end
 
