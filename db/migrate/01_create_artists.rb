62b5f49c1b9df485a0d50039c57fac72094553e2
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-22 23:00:07 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,18 +1,4 @@
 class CreateArtists < ActiveRecord::Migration
-
-#def create_table
-  sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS artists (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      genre TEXT,
-      age INTEGER,
-      hometown TEXT
-      )
-      SQL
-      ActiveRecord::Base.connection.execute(sql)
-    end
-
   def up
   end
 
